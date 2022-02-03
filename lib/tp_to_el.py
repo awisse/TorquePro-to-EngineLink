dbf69eeb9b9ec004528f71b87c2bf87f1846f948
Aurel Wisse <4907846+awisse@users.noreply.github.com>
2022-02-03 12:04:48 -0500

    Added conversion of shift (a<8)
    Plus:
    * Comments that specify application of program
    * Check path of option --target-dir
    * Added .ipynb_checkpoints to .gitignore

--- lib/tp_to_el.py
+++ lib/tp_to_el.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # vim: fileencoding=utf-8
 #
+# This program converts TorquePro exdended PID files from the site
+# (https://github.com/JejuSoul/OBD-PIDs-for-HKMC-EVs)
+# into extended PID files for EngineLink
+# (http://outdoor-apps.com/enginelink.html)
 """
 Import a TorquePro PID file and transform it into an EngineLink PID file.
 
@@ -29,6 +33,7 @@ COPY_FIELDS = [PID, DESC, MIN, MAX, UNIT, ECU]
 BIT_RE = r'Bit\((?P<bit>[A-Za-z]{1,2}:[0-7])\)'
 INT16_RE = r'Int16\(([A-Z]{1,2}):([A-Z]{1,2})\)'
 INT32_RE = r'Int32\(([A-Z]{1,2}):([A-Z]{1,2}):([A-Z]{1,2}):([A-Z]{1,2})\)'
+SHIFT_RE = r'\(([A-Z]{1,2})<(8|16|24)\)'
 SIGNED_RE = r'Signed(?P<signed>\([A-Za-z]{1,2}\))'
 
 def int16_conv(matchobj):
@@ -49,6 +54,14 @@ def int32_conv(matchobj):
     formula = '((({}*256+{})*256+{})*256+{})'.format(*matchobj.groups())
     return formula
 
+def shift_conv(matchobj):
+    """
+    Transform '(a<b)' into 'a * 2**b'.
+    """
+    shift = 1 << int(matchobj.group(2))
+    formula = '{}*{}'.format(matchobj.group(1), shift)
+    return formula
+
 def torquepro_to_enginelink_formula(tp_formula, variables):
     """
     Replace variable names in the `tp_formula` with names accepted
@@ -56,11 +69,6 @@ def torquepro_to_enginelink_formula(tp_formula, variables):
     """
     # Make a copy
     el_formula = str(tp_formula)
-
-    # Replace variable names
-    for var in variables:
-        tp_var = f'val{{{var}}}'
-        el_formula = el_formula.replace(tp_var, f'[{variables[var]}]')
 
     # Replace `Signed` function
     el_formula = re.sub(SIGNED_RE, 'SIGNED\g<signed>', el_formula)
@@ -73,6 +81,9 @@ def torquepro_to_enginelink_formula(tp_formula, variables):
 
     # Replace `Int32` function
     el_formula = re.sub(INT32_RE, int32_conv, el_formula, flags=re.IGNORECASE)
+
+    # Replace `<` (bit shift)
+    el_formula = re.sub(SHIFT_RE, shift_conv, el_formula, flags=re.IGNORECASE)
 
     return el_formula
 
@@ -88,9 +99,26 @@ def torquepro_to_enginelink_row(row, variables):
     # Get the variable name and define EngineLink variable
     variables[row[VAR]] = row[DESC]
 
-    # Get the EngineLink formula from the TorquePro formula
-    engine_link[FORMULA] = torquepro_to_enginelink_formula(row[FORMULA],
-                                                           variables)
+    formula = row[FORMULA]
+
+    # Replace variable names
+    var_replacements = 0
+    for var in variables:
+        tp_var = f'val{{{var}}}'
+        # Use re.subn in order to recover number of replacements
+        formula, replaced = re.subn(tp_var, f'[{variables[var]}]',
+                                    formula)
+        var_replacements += replaced
+
+    # If there is a tp_formula with variables, EngineLink doesn't want a PID
+    # and no values from the scanner are used.
+    if var_replacements > 0:
+        engine_link[PID] = ''
+        engine_link[FORMULA] = formula
+    else:
+        # Get the EngineLink formula from the TorquePro formula
+        engine_link[FORMULA] =\
+            torquepro_to_enginelink_formula(formula, variables)
     return engine_link
 
 def filetype_argument(arg):
@@ -106,6 +134,16 @@ def filetype_argument(arg):
 
     return fpath
 
+def dir_argument(arg):
+    """
+    Check whether argument is a directory.
+    """
+    dir_arg = Path(arg)
+    if not dir_arg.is_dir():
+        raise argparse.ArgumentError('--target-dir must be a directory')
+
+    return dir_arg
+
 def prepare_options():
     """
     Prepare the option parser.
@@ -119,6 +157,7 @@ def prepare_options():
     parser.add_argument('--target-dir', '-d',
                         help=('The directory where the transformed file will '
                               'be saved. Default: "."'),
+                        type=dir_argument,
                         default=Path('.'))
 
     return parser
@@ -138,7 +177,7 @@ def main():
     tp_path = args.tp_file
 
     el_name = f'{Path(args.tp_file).stem}-EL.csv'
-    el_path = Path(args.target_dir).joinpath(el_name)
+    el_path = args.target_dir.joinpath(el_name)
 
     # Opening TorquePro file for reading
     with open(tp_path, mode='r', errors='replace',
